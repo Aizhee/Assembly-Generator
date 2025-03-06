68d3c67e21732764b5c703380a56ac50ce367c2c
Aizhee <atjamilano@slsu.edu.ph>
2025-03-06 15:10:40 +0800

    le update

--- string_print_using_var.py
+++ string_print_using_var.py
@@ -10,25 +10,24 @@ def generate_assembly(input_file, output_file):
     
     with open(output_file, 'w') as file:
         file.write(".model small\n")
-        file.write(".stack 100h\n")
-        file.write(".data\n")
+        file.write(".data\n\n")
         
         for i, line in enumerate(lines):
             line = line.strip()
-            file.write(f"message{i} db '{line}', 13, 10, '$'\n")  # Store each line with CRLF and '$' for printing
+            file.write(f"var{i} db '{line}', 13, 10, '$'\n")  # Store each line with CRLF and '$' for printing
         
-        file.write(".code\n")
+        file.write("\n.code\n")
         file.write("start:\n")
-        file.write("    mov ax, @data\n")
-        file.write("    mov ds, ax\n")
-        file.write("    mov ah, 09h\n")
+        file.write("mov ax, @data\n")
+        file.write("mov ds, ax\n")
+        file.write("mov ah, 09h\n\n")
         
         for i in range(len(lines)):
-            file.write(f"    mov dx, offset message{i}\n")
-            file.write("    int 21h\n")
+            file.write(f"mov dx, offset var{i}\n")
+            file.write("int 21h\n")
         
-        file.write("    mov ah, 4Ch\n")
-        file.write("    int 21h\n")
+        file.write("\nmov ah, 4Ch\n")
+        file.write("int 21h\n")
         file.write("end start\n")
 
 
